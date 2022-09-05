52d96ff51876e874bb8ecbc2673193241601e04f
Dave van der Merwe <39761531+davevdm1968@users.noreply.github.com>
2022-09-05 14:11:00 +0200

    Create deleteavailablevolumes.py

---
+++ deleteavailablevolumes.py
@@ -0,0 +1,63 @@
+# Delete Volume Available older than
+#
+# @author Dave van der Merwe
+#
+# This script will search for all volumes that are in an available state 
+# If the volume was detached more than older_than_days variable it will delete it
+#
+import boto3
+from datetime import datetime, timezone
+#
+older_than_days = 30 #
+#
+def detachdays(client, volume):
+    try:
+        response = client.lookup_events(
+            LookupAttributes=[
+                {
+                    'AttributeKey': 'EventName',
+                    'AttributeValue': 'DetachVolume'
+                },
+                {
+                    'AttributeKey': 'ResourceName',
+                    'AttributeValue': volume
+                },
+            ],
+            #StartTime=datetime(2015, 1, 1),
+            #EndTime=datetime(2015, 1, 1),
+            #EventCategory='insight',
+            MaxResults=1,
+            #NextToken='string'
+        )
+        detachdate = response['Events'][0]['EventTime']
+        print(f'{volume} detached {detachdate}')
+        delta = datetime.now(timezone.utc) - detachdate
+        return delta.days
+    except:
+        print(f'Unable to calculate detachdate for {volume}')
+        return 0
+
+def lambda_handler(object, context):
+
+    # Get list of regions
+    ec2_client = boto3.client('ec2')
+    regions = [region['RegionName']
+               for region in ec2_client.describe_regions()['Regions']]
+
+    for region in regions:
+        if (region != 'eu-west-1'):
+            continue
+        ec2 = boto3.resource('ec2', region_name=region)
+        client = boto3.client('cloudtrail', region_name=region)
+        print("Region:", region)
+
+        # List only unattached volumes ('available' vs. 'in-use')
+        volumes = ec2.volumes.filter(
+            Filters=[{'Name': 'status', 'Values': ['available']}])
+
+        for volume in volumes:
+            v = ec2.Volume(volume.id)
+            if (detachdays(client, volume.id) > older_than_days):
+                print("Deleting EBS volume: {}, Size: {} GiB".format(v.id, v.size))
+                #v.delete()
+#
